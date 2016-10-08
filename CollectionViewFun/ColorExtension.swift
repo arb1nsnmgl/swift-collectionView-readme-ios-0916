0eca9ea3fc0b97f694ff85595d5209825f128ae4
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-10-08 11:41:02 -0400

    Completed

--- CollectionViewFun/ColorExtension.swift
+++ CollectionViewFun/ColorExtension.swift
@@ -14,7 +14,7 @@ extension UIColor{
     class func getRandomColor() -> UIColor{
         
         let red = CGFloat(drand48())
-        let green = CGFloat(drand48())
+        let green = CGFloat(drand48()) 
         let blue = CGFloat(drand48())
         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
         
